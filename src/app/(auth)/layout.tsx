d48574d49516b484db38aafd472c349081aa33e3
Shivam Shahi <shivam.shahi06@gmail.com>
2024-12-20 17:12:47 +0530

    stacked cards

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,23 @@
+import StackedCards from "@/components/StackedCards";
+import Image from "next/image";
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="w-screen h-screen fixed grid grid-cols-1 lg:grid-cols-2">
+      {/* Static Images */}
+      <div className="w-full h-full hidden lg:flex items-center justify-center border-r border-[#D8DAE5] bg-[#FFFFFF]">
+        <StackedCards />
+        <img
+          src={"/logoFaded.png"}
+          alt={"logo"}
+          className={`absolute left-0 bottom-0 w-[300px] aspect-square`}
+        />
+      </div>
+
+      {/* Auth Form with Tabs */}
+      <div className="w-full px-4 py-6 bg-[#FAFAFA]">{children}</div>
+    </div>
+  );
+};
+
+export default AuthLayout;
--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-
 import { Button } from "./ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
